a98cdaa7984459994f1e4853c1c4a58c8d6f9d68
swlee <swo98@naver.com>
2025-07-06 23:05:26 +0900

    init: initial setting

---
+++ src/main/java/back2basics/logbackasynctest/LogbackAsyncTestApplication.java
@@ -0,0 +1,13 @@
+package back2basics.logbackasynctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackAsyncTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackAsyncTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/back2basics/logbackasynctest/LogbackAsyncTestApplicationTests.java
@@ -0,0 +1,13 @@
+package back2basics.logbackasynctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackAsyncTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
